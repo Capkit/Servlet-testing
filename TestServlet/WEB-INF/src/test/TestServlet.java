5fc9f29e9f0939763dab2948f5267ea81a9c60de
Kitti Füredi <40038606+Capkit@users.noreply.github.com>
2019-04-17 21:44:16 +0200

    Update TestServlet.java

--- TestServlet/WEB-INF/src/test/TestServlet.java
+++ TestServlet/WEB-INF/src/test/TestServlet.java
@@ -15,7 +15,6 @@ public class TestServlet extends HttpServlet {
 				   
       // Set the response message's MIME type
       response.setContentType("text/html;charset=UTF-8");
-      // Allocate a output writer to write the response message into the network socket
       PrintWriter out = response.getWriter();
  
       // Write the response message, in an HTML page
@@ -25,18 +24,18 @@ public class TestServlet extends HttpServlet {
          out.println("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
          out.println("<title>Hello</title></head>");
          out.println("<body>");
-         out.println("<h1>Hello</h1>");  // says Hello
+         out.println("<h1>Hello</h1>"); 
          // Echo client's request information
          out.println("<p>Request URI: " + request.getRequestURI() + "</p>");
          out.println("<p>Protocol: " + request.getProtocol() + "</p>");
          out.println("<p>PathInfo: " + request.getPathInfo() + "</p>");
          out.println("<p>Remote Address: " + request.getRemoteAddr() + "</p>");
-         // Generate a random number upon each request
+    
          out.println("<p>A Random Number: <strong>" + Math.random() + "</strong></p>");
          out.println("</body>");
          out.println("</html>");
       } finally {
-         out.close();  // Always close the output writer
+         out.close();  
       }
    }
 }
